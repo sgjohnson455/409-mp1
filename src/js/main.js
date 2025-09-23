6a0e013ab5de0436a7d62a14fd4d75d0af1bf3a7
sjohn241 <sjohn241@illinois.edu>
2025-09-23 12:38:31 -0500

    Tweaked navbar properties and links, added section js for navbar highlighting, and added specific styling for sections of webpage

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,27 @@
 /* Your JS here. */
-console.log('Hello World!')
+// console.log('Hello World!')
+
+// select sections to look for and navbar object
+const sections = document.querySelectorAll('.header, .section, .footer');
+const navItems = document.querySelectorAll('#navbar a');
+
+// Interaction Observer - tracks objects in the viewport
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // remove active/clicking from relevant link in navbar
+            navItems.forEach(link => link.classList.remove('active'));
+
+            // find matching section in navbar and update to active
+            const activeLink = document.querySelector(`#navbar a[href="#${entry.target.id}"]`);
+            if (activeLink) {
+                activeLink.classList.add('active');
+            }
+        }
+    });
+}, { threshold: 0.5 }); // 50% threshold for section visibility
+
+// observe every section in viewport for InteractionObserver
+sections.forEach(section => {
+    observer.observe(section);
+});
